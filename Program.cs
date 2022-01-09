f78c1d0f0c93c963bfa2b22b3219b4d6d2410ff9
talkofice <talkofice@gmail.com>
2022-01-09 17:20:47 +0800

    modify appsetting

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -30,9 +33,17 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+string vPath = app.Configuration["virtualPath"];
+
 app.UseHttpLogging(); //增加日志记录
 
-string vPath = app.Configuration["virtualPath"];
+// global cors policy
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials()); // allow credentials
+
 app.UsePathBase(new PathString(vPath));
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
