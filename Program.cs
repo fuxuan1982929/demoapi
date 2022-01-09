500a4d44efb7c6a75b2e11e702946f9afd7a77a1
talkofice <talkofice@gmail.com>
2022-01-09 12:28:07 +0800

    edit swagger

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "DEMO API",
+        Description = "An ASP.NET Core Web API for managing ",
+        TermsOfService = new Uri("https://api.talkofice.com/demoapi"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://api.talkofice.com/license")
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -15,7 +36,12 @@ app.UseHttpLogging(); //增加日志记录
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        //若为string.Empty,则为根目录
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.Use(async (context, next) =>
